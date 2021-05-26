4e4b23277462c86b6913c4e6b208fb9259ae7b88
juan <juanan5febrero@gmail.com>
2021-05-26 16:40:20 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 import { NavComponent } from './nav/nav.component';
+=======
+import { HeaderComponent } from './header/header.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     NavComponent
+=======
+    HeaderComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
