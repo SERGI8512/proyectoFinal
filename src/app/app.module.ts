63585c3f1405f626f3192dc0d57c617f9bba6ee5
juan <juanan5febrero@gmail.com>
2021-06-08 17:50:38 +0200

    arreglo error nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { NavComponent } from './components/nav/nav.component';
+
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { CuidadoresComponent } from './components/cuidadores/cuidadores.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
@@ -23,7 +23,7 @@ import { DestinosComponent } from './components/destinos/destinos.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent,
+    
     NavBarComponent,
     CuidadoresComponent,
     UsuariosComponent,
