9cc38c0ef888f8d7072095234f4fc0074ff1f492
sergi8512 <sergi8512@gmail.com>
2021-06-17 00:18:30 +0200

    Modificación Forms con maquetación nueva y creación components/Perfiles
    Hemos "reenganchado" las peticiones post con la nueva maquetación de los formularios sobre:
    
    - api/mascotas/new
    - api/usuarios/new
    - api/cuidadores/new

---
+++ src/app/components/Perfiles/perfil-mascota/perfil-mascota.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-mascota',
+  templateUrl: './perfil-mascota.component.html',
+  styleUrls: ['./perfil-mascota.component.css']
+})
+export class PerfilMascotaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
